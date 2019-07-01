6822d0c7a2e03bcb4b5693f38da3f84d2773ab00
kevinmarais <kevin@24bitgames.com>
2019-07-01 10:20:33 +0200

    Cleaned up and documented code

--- AndroidProject/app/src/main/java/com/unity/mynativeapp/MainActivity.java
+++ AndroidProject/app/src/main/java/com/unity/mynativeapp/MainActivity.java
@@ -7,44 +7,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
-    static int CurrentSelectedItem;
+public class MainActivity extends AppCompatActivity {
+    static int CurrentSelectedItem; // 0 = mug, 1 = shirt.
+
+    // Shop item color configurations. 0 = mug, 1 = shirt.
     static int[] ItemConfigs = new int[2];
 
     static int[] Colors = {Color.parseColor("#FFFFFF"),
-                           Color.parseColor("#EC1746"),
-                           Color.parseColor("#04BAD2"),
-                           Color.parseColor("#CADA29")};
-
-    static int[] MugImages = {R.drawable.mugwhite,
-                              R.drawable.mugmagenta,
-                              R.drawable.mugcyan,
-                              R.drawable.muglime};
-
-    static int[] ShirtImages = {R.drawable.shirtwhite,
-                                R.drawable.shirtmagenta,
-                                R.drawable.shirtcyan,
-                                R.drawable.shirtlime};
+            Color.parseColor("#EC1746"),
+            Color.parseColor("#04BAD2"),
+            Color.parseColor("#CADA29")};
 
     static String[] ColorsStrings = {"White", "Magenta", "Cyan", "Lime"};
 
+    static int[] MugImages = {R.drawable.mugwhite,
+            R.drawable.mugmagenta,
+            R.drawable.mugcyan,
+            R.drawable.muglime};
+
+    static int[] ShirtImages = {R.drawable.shirtwhite,
+            R.drawable.shirtmagenta,
+            R.drawable.shirtcyan,
+            R.drawable.shirtlime};
+
     static int NumberOfColors = 4;
 
-    public static int GetNextColorForCurrent()
-    {
+    public static int GetNextColorForCurrentItem() {
         return Colors[(ItemConfigs[CurrentSelectedItem] + 1) % MainActivity.NumberOfColors];
     }
 
-    public static String GetColorStringForCurrent()
-    {
+    public static String GetColorStringForCurrentItem() {
         return ColorsStrings[ItemConfigs[CurrentSelectedItem]];
     }
 
-    public static void IncCurrentConfig()
-    {
+    public static void IncCurrentItemConfig() {
         ItemConfigs[CurrentSelectedItem] = (ItemConfigs[CurrentSelectedItem] + 1) % MainActivity.NumberOfColors;
     }
 
@@ -56,22 +53,24 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        View v0 = findViewById(R.id.previewbutton0);
-        v0.setOnClickListener(new View.OnClickListener() {
+        // assign on click listeners to the AR Preview buttons
+        View mugPreviewButton = findViewById(R.id.previewbutton0);
+        mugPreviewButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectShopItem(0);
             }
         });
 
-        View v1 = findViewById(R.id.previewbutton1);
-        v1.setOnClickListener(new View.OnClickListener() {
+        View shirtPreviewButton = findViewById(R.id.previewbutton1);
+        shirtPreviewButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectShopItem(1);
             }
         });
 
-        View imageView0 = findViewById(R.id.mugColorChanger);
-        imageView0.setOnClickListener(new View.OnClickListener() {
+        // assign the correct display image for the shop items and assign on click listeners.
+        View mugColorButton = findViewById(R.id.mugColorChanger);
+        mugColorButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 IncConfig(0);
             }
@@ -79,10 +78,10 @@ public class MainActivity extends AppCompatActivity
         ImageView mugDisplay = findViewById(R.id.mugDisplay);
         mugDisplay.setImageResource(MugImages[ItemConfigs[0]]);
         ImageView mugColorChangeButton = findViewById(R.id.mugColorChanger);
-        UpdateColorButton(mugColorChangeButton, ItemConfigs[0] );
+        UpdateColorButton(mugColorChangeButton, ItemConfigs[0]);
 
-        View imageView1 = findViewById(R.id.shirtColorChanger);
-        imageView1.setOnClickListener(new View.OnClickListener() {
+        View shirtColorButton = findViewById(R.id.shirtColorChanger);
+        shirtColorButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 IncConfig(1);
             }
@@ -93,44 +92,29 @@ public class MainActivity extends AppCompatActivity
         UpdateColorButton(shirtColorChangeButton, ItemConfigs[1]);
     }
 
-    private void selectShopItem(int i)
-    {
+    // Start the Unity Activity.
+    private void selectShopItem(int i) {
         CurrentSelectedItem = i;
         Intent intent = new Intent(this, MainUnityActivity.class);
         startActivity(intent);
     }
 
-    private void IncConfig(int i)
-    {
-        int config = (ItemConfigs[i] + 1) % NumberOfColors;
-        ItemConfigs[i] = config;
+    // Increments the configuration (color setting) for a given item. 0 = mug, 1 = shirt.
+    private void IncConfig(int itemIndex) {
+        int config = (ItemConfigs[itemIndex] + 1) % NumberOfColors;
+        ItemConfigs[itemIndex] = config;
 
-        ImageView v = findViewById(i == 0 ? R.id.mugDisplay : R.id.shirtDisplay);
-        v.setImageResource(i == 0 ? MugImages[config] : ShirtImages[config]);
+        ImageView v = findViewById(itemIndex == 0 ? R.id.mugDisplay : R.id.shirtDisplay);
+        v.setImageResource(itemIndex == 0 ? MugImages[config] : ShirtImages[config]);
 
         // show next colour
-        ImageView colorChangeButton = findViewById(i == 0 ? R.id.mugColorChanger : R.id.shirtColorChanger);
+        ImageView colorChangeButton = findViewById(itemIndex == 0 ? R.id.mugColorChanger : R.id.shirtColorChanger);
         UpdateColorButton(colorChangeButton, config);
     }
 
-    private void UpdateColorButton(ImageView colorButton, int colorIndex)
-    {
+    private void UpdateColorButton(ImageView colorButton, int colorIndex) {
         int next = (colorIndex + 1) % 4;
         colorButton.setImageResource(next > 0 ? R.drawable.colour_button : R.drawable.colour_button_white);
         colorButton.setColorFilter(next > 0 ? Colors[next] : Color.TRANSPARENT);
-        MainUnityActivity.UpdateColorButton();
-    }
-
-    public void onUnityUnload(View v) {
-        if(MainUnityActivity.instance != null)
-            MainUnityActivity.instance.finish();
-        else showToast("Show Unity First");
-    }
-
-    public void showToast(String message) {
-        CharSequence text = message;
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-        toast.show();
     }
 }
--- AndroidProject/app/src/main/java/com/unity/mynativeapp/MainUnityActivity.java
+++ AndroidProject/app/src/main/java/com/unity/mynativeapp/MainUnityActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.Display;
 import android.view.View;
 import android.widget.ImageButton;
@@ -15,7 +13,7 @@ import com.company.product.OverrideUnityActivity;
 
 public class MainUnityActivity extends OverrideUnityActivity {
 
-    static ImageButton colorButton;
+    ImageButton colorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,85 +21,70 @@ public class MainUnityActivity extends OverrideUnityActivity {
         addControlsToUnityFrame();
     }
 
+    // Send messages to Unity to update the AR item.
     @Override
     protected void updateUnityShopItem() {
-        UnitySendMessage("AR Session Origin", "SetProduct", Integer.toString(MainActivity.CurrentSelectedItem ));
-        UnitySendMessage("AR Session Origin", "SetColor", MainActivity.GetColorStringForCurrent());
+        UnitySendMessage("AR Session Origin", "SetProduct", Integer.toString(MainActivity.CurrentSelectedItem));
+        UnitySendMessage("AR Session Origin", "SetColor", MainActivity.GetColorStringForCurrentItem());
     }
 
+    // Called from the Unity Activity. Makes the color change button visible.
     @Override
     protected void itemPlacedInAR() {
-        new Thread(mMessageSender).start();
+        MainUnityActivity.this.runOnUiThread(new Runnable() {
+            public void run() {
+                colorButton.setColorFilter(MainActivity.GetNextColorForCurrentItem());
+                colorButton.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
-    private static Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            colorButton.setVisibility(View.VISIBLE);
-        }
-    };
+    public void addControlsToUnityFrame() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        int size = (point.x / 2);
 
-    private final Runnable mMessageSender = new Runnable() {
-        public void run() {
-            mHandler.sendMessage( mHandler.obtainMessage());
-        }
-    };
+        ImageButton backButton = new ImageButton(this);
+        backButton.setImageResource(R.drawable.button_back);
+        backButton.setBackgroundColor(Color.TRANSPARENT);
+        backButton.setScaleType(ImageView.ScaleType.FIT_START);
+        backButton.setX(10);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showMainActivity();
+                colorButton.setVisibility(View.GONE);
+            }
+        });
+        getUnityFrameLayout().addView(backButton, size, size / 2);
 
 
-    protected void showMainActivity() {
+        colorButton = new ImageButton(this);
+        colorButton.setImageResource(R.drawable.button_colour);
+        colorButton.setBackgroundColor(Color.TRANSPARENT);
+        colorButton.setScaleType(ImageView.ScaleType.FIT_START);
+        colorButton.setX(point.x - size * 0.8f);
+        colorButton.setColorFilter(MainActivity.GetNextColorForCurrentItem());
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onColorButtonPressed();
+            }
+        });
+        colorButton.setVisibility(View.GONE);
+        getUnityFrameLayout().addView(colorButton, size, size / 2);
+    }
+
+    // Return to the MainActivity. Sends a message to the Unity Activity to clear the placed AR item.
+    private void showMainActivity() {
         UnitySendMessage("AR Session Origin", "Clear", "");
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public static void UpdateColorButton()
-    {
-        if (colorButton != null)
-        {
-            colorButton.setColorFilter(MainActivity.GetNextColorForCurrent());
-        }
-    }
-
-    public void addControlsToUnityFrame() {
-            Display display = getWindowManager(). getDefaultDisplay();
-            Point point = new Point();
-            display.getSize(point);
-            int size = (point.x / 2);
-
-            ImageButton backButton = new ImageButton(this);
-            backButton.setImageResource(R.drawable.button_back);
-            backButton.setBackgroundColor(Color.TRANSPARENT);
-            backButton.setScaleType(ImageView.ScaleType.FIT_START);
-            backButton.setX(10);
-
-            backButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    showMainActivity();
-                    colorButton.setVisibility(View.GONE);
-                }
-            });
-            getUnityFrameLayout().addView(backButton, size, size / 2);
-
-
-            colorButton = new ImageButton(this);
-            colorButton.setImageResource(R.drawable.button_colour);
-            colorButton.setBackgroundColor(Color.TRANSPARENT);
-            colorButton.setScaleType(ImageView.ScaleType.FIT_START);
-            colorButton.setX(point.x - size * 0.8f);
-            colorButton.setColorFilter(MainActivity.GetNextColorForCurrent());
-            colorButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    onColorButtonPressed();
-                }
-            });
-            colorButton.setVisibility(View.GONE);
-            getUnityFrameLayout().addView(colorButton, size, size / 2);
-    }
-
-    private void onColorButtonPressed()
-    {
-        MainActivity.IncCurrentConfig();
-        UpdateColorButton();
+    private void onColorButtonPressed() {
+        MainActivity.IncCurrentItemConfig();
+        colorButton.setColorFilter(MainActivity.GetNextColorForCurrentItem());
         updateUnityShopItem();
     }
 }
